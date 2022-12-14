c0ce752ebdd0e7e362a8434a714bb1d67e83b228
phankhanh6 <105293837+phankhanh6@users.noreply.github.com>
2022-12-15 02:30:30 +0700

    login form

---
+++ Menu.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace QuanLyPX
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.xeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xeNhậpKhẩuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhSáchThợToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nướcSảnXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.danhSáchNSXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.phụKiệnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýKhoPhụKiệnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.phânCôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bảngPhânCôngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thànhPhầnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xeToolStripMenuItem,
+            this.thoToolStripMenuItem,
+            this.nướcSảnXuấtToolStripMenuItem,
+            this.phụKiệnToolStripMenuItem,
+            this.phânCôngToolStripMenuItem,
+            this.thànhPhầnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(716, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // xeToolStripMenuItem
+            // 
+            this.xeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xeNhậpKhẩuToolStripMenuItem});
+            this.xeToolStripMenuItem.Name = "xeToolStripMenuItem";
+            this.xeToolStripMenuItem.Size = new System.Drawing.Size(40, 24);
+            this.xeToolStripMenuItem.Text = "Xe";
+            // 
+            // xeNhậpKhẩuToolStripMenuItem
+            // 
+            this.xeNhậpKhẩuToolStripMenuItem.Name = "xeNhậpKhẩuToolStripMenuItem";
+            this.xeNhậpKhẩuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.xeNhậpKhẩuToolStripMenuItem.Text = "Danh sách xe";
+            // 
+            // thoToolStripMenuItem
+            // 
+            this.thoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhSáchThợToolStripMenuItem});
+            this.thoToolStripMenuItem.Name = "thoToolStripMenuItem";
+            this.thoToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.thoToolStripMenuItem.Text = "Thợ";
+            // 
+            // danhSáchThợToolStripMenuItem
+            // 
+            this.danhSáchThợToolStripMenuItem.Name = "danhSáchThợToolStripMenuItem";
+            this.danhSáchThợToolStripMenuItem.Size = new System.Drawing.Size(186, 26);
+            this.danhSáchThợToolStripMenuItem.Text = "Danh sách thợ";
+            // 
+            // nướcSảnXuấtToolStripMenuItem
+            // 
+            this.nướcSảnXuấtToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhSáchNSXToolStripMenuItem});
+            this.nướcSảnXuấtToolStripMenuItem.Name = "nướcSảnXuấtToolStripMenuItem";
+            this.nướcSảnXuấtToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
+            this.nướcSảnXuấtToolStripMenuItem.Text = "Nước sản xuất";
+            // 
+            // danhSáchNSXToolStripMenuItem
+            // 
+            this.danhSáchNSXToolStripMenuItem.Name = "danhSáchNSXToolStripMenuItem";
+            this.danhSáchNSXToolStripMenuItem.Size = new System.Drawing.Size(192, 26);
+            this.danhSáchNSXToolStripMenuItem.Text = "Danh sách NSX";
+            // 
+            // phụKiệnToolStripMenuItem
+            // 
+            this.phụKiệnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quảnLýKhoPhụKiệnToolStripMenuItem});
+            this.phụKiệnToolStripMenuItem.Name = "phụKiệnToolStripMenuItem";
+            this.phụKiệnToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.phụKiệnToolStripMenuItem.Text = "Phụ kiện";
+            // 
+            // quảnLýKhoPhụKiệnToolStripMenuItem
+            // 
+            this.quảnLýKhoPhụKiệnToolStripMenuItem.Name = "quảnLýKhoPhụKiệnToolStripMenuItem";
+            this.quảnLýKhoPhụKiệnToolStripMenuItem.Size = new System.Drawing.Size(230, 26);
+            this.quảnLýKhoPhụKiệnToolStripMenuItem.Text = "Quản lý kho phụ kiện";
+            // 
+            // phânCôngToolStripMenuItem
+            // 
+            this.phânCôngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bảngPhânCôngToolStripMenuItem});
+            this.phânCôngToolStripMenuItem.Name = "phânCôngToolStripMenuItem";
+            this.phânCôngToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.phânCôngToolStripMenuItem.Text = "Phân công";
+            // 
+            // bảngPhânCôngToolStripMenuItem
+            // 
+            this.bảngPhânCôngToolStripMenuItem.Name = "bảngPhânCôngToolStripMenuItem";
+            this.bảngPhânCôngToolStripMenuItem.Size = new System.Drawing.Size(200, 26);
+            this.bảngPhânCôngToolStripMenuItem.Text = "Bảng phân công";
+            // 
+            // thànhPhầnToolStripMenuItem
+            // 
+            this.thànhPhầnToolStripMenuItem.Name = "thànhPhầnToolStripMenuItem";
+            this.thànhPhầnToolStripMenuItem.Size = new System.Drawing.Size(100, 24);
+            this.thànhPhầnToolStripMenuItem.Text = "Thành phần";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.groupBox1.BackgroundImage = global::QuanLyPX.Properties.Resources.nền;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(716, 347);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(85, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(564, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phân Xưởng Lắp Ráp Oto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 366);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem xeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xeNhậpKhẩuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nướcSảnXuấtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem phụKiệnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem phânCôngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhSáchThợToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem danhSáchNSXToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýKhoPhụKiệnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bảngPhânCôngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thànhPhầnToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PhanXuong.sql
@@ -0,0 +1,87 @@
+﻿--tạo database và các bảng
+CREATE DATABASE PhanXuong
+go
+use PhanXuong
+go
+if EXISTS (select * from sysobjects where name='tNuocSX')
+drop table tNuocSX
+go
+create TABLE tNuocSX
+(
+	manuoc char(6) not null PRIMARY key,
+	tennuoc nvarchar(30)
+)
+if EXISTS (select * from sysobjects where name='tXe')
+drop table tXe
+go
+create TABLE tXe
+(
+	maxe char(4) not null PRIMARY key,
+	tenxe nvarchar(30),
+	ghichu nvarchar(30)
+)
+if EXISTS (select * from sysobjects where name='tTho')
+drop table tTho
+go
+create TABLE tTho
+(
+	matho char(4) not null PRIMARY key,
+	tentho nvarchar(30),
+	bactho nvarchar(20)
+)
+if exists (SELECT * from sysobjects where name='tPhuKien')
+drop TABLE tPhuKien
+go
+CREATE TABLE tPhuKien
+(
+	mapk char(4) not null PRIMARY key,
+	tenpk nvarchar(40),
+	manuoc char(6),
+	dongia float,
+	soluongtontai int,
+	ghichu nvarchar(40)
+FOREIGN key (manuoc) REFERENCES tNuocSX (manuoc)
+)
+if EXISTS (select * from sysobjects where name='tThanhPhan')
+drop table tThanhPhan
+go
+create TABLE tThanhPhan
+(
+	mathanhphan char(4) not null primary key,
+	maxe char(4),
+	mapk char(4),
+	donvitinh nvarchar(10) DEFAULT 'Cái',
+	soluong int
+	foreign key (maxe) REFERENCES tXe (maxe),
+	FOREIGN key (mapk) REFERENCES tPhuKien (mapk)
+)
+if EXISTS (select * from sysobjects where name='tPhanCong')
+drop table tPhanCong
+go
+create TABLE tPhanCong
+(
+	maphancong char(4) not null primary key,
+	matho char(4),
+	maxe char(4)
+	FOREIGN key (matho) REFERENCES tTho (matho),
+	FOREIGN key (maxe) REFERENCES tXe (maxe)
+)
+--thêm dữ liệu vào các bảng
+insert into tNuocSX values ('EURO01',N'France'),
+('ASIA01',N'Japan'),
+('ASIA02',N'Việt Nam')
+INSERT into tXe VALUES ('AU11',N'Audi Logale',N'thuế giá trị tăng cao'),
+('VI10',N'Vinfast Lux 2.0',N'xe nội'),
+('GE09',N'Volkahaghen',N'xe nhập khẩu')
+insert into tTho values ('MS01',N'Nguyễn Văn Kiều',N'Diamond'),
+('MS02',N'Albert Chel',N'Master'),
+('MS03',N'Du Hiểu Ký',N'Platinum')
+Insert into tPhuKien values ('PK10',N'lốp xe','EURO01',25000000,10,N'Lốp xe nhập từ Châu Âu'),
+('PK12',N'đèn xe','ASIA01',15500000,2,N'đèn xe jin Nhật Bản'),
+('PK11',N'mâm xe','EURO01',100000000,14,N'mâm xe độ')
+INSERT into tPhanCong values ('PC01','MS01','VI10'),
+('PC02','MS02','VI10'),
+('PC03','MS03','GE09')
+Insert into tThanhPhan VALUES ('TP01','GE09','PK10',N'cái',2),
+('TP02','VI10','PK12',N'cái',4),
+('TP03','VI10','PK11',N'cái',4)
